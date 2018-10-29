3624df985ba072031d0bd248aeed71f8254731f9
ageekymonk <ramzthecoder@gmail.com>
2018-10-30 09:47:08 +1100

    Adding more tests for ecloud state

--- test/ecloud-state-test.el
+++ test/ecloud-state-test.el
@@ -10,11 +10,30 @@
 (require 'buttercup)
 
 (describe "State on initialization"
+          (before-each
+           (ecloud-state-init))
+          
           (it "should be empty"
-              (ecloud-state-init)
               (expect (ht-size (ecloud-state)) :to-equal 0))
 
           (it "Should have a cache directory created"
-              (ecloud-state-init)
               (expect (f-dir? (format "%s/ecloud" pcache-directory)))))
 
+(describe "When registering cloud"
+          (before-each
+           (ecloud-register-cloud "azure"))
+
+          (it "state for the cloud should be initialized"
+              (expect (ht-size (ecloud-state)) :to-equal 1)
+              (expect (ecloud-get-cloud-state "azure") :not :to-equal nil)))
+
+(describe "Wen registering resource type"
+          (before-each
+           (ecloud-register-resource-type "azure" "vnet"))
+
+          (it "state for the cloud should be initialized"
+              (expect (ht-size (ecloud-state)) :to-equal 1)
+              (expect (ht-get (ecloud-state) "azure") :not :to-equal nil))
+
+          (it "Resource type for the cloud should be initialized"
+              (expect (ecloud-get-resource-type-state "azure" "vnet") :not :to-equal nil)))
